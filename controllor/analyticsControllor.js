f80b46660184a5a2d8c2021faf565e2b2802d48e
bikashroy120 <bikashroydt@gmail.com>
2023-09-28 23:42:55 +0600

    add

--- controllor/analyticsControllor.js
+++ controllor/analyticsControllor.js
@@ -2,8 +2,9 @@ import { catchAsyncErrors } from "../middleware/catchAsyncErrors.js";
 import userModal from "../models/userModel.js";
 import ErrorHandler from "../utils/ErrorHandlers.js";
 import { generateLast12MonthData } from "../utils/analyticsGenerator.js";
+import courseModal from "../models/courseModel.js";
 
-
+// get user Analyser --- for admin
 export const getUserAnalytics = catchAsyncErrors(async(req,res,next)=>{
     try {
         const users = await generateLast12MonthData(userModal)
@@ -11,6 +12,21 @@ export const getUserAnalytics = catchAsyncErrors(async(req,res,next)=>{
         res.status(200).json({
             success:true,
             users
+        })
+    } catch (error) {
+        next(new ErrorHandler(error.message, 500))
+    }
+})
+
+
+// get user course --- for admin
+export const getCourseAnalytics = catchAsyncErrors(async(req,res,next)=>{
+    try {
+        const course = await generateLast12MonthData(courseModal)
+
+        res.status(200).json({
+            success:true,
+            course
         })
     } catch (error) {
         next(new ErrorHandler(error.message, 500))
--- routes/analyticsRoute.js
+++ routes/analyticsRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
 import { isAdmin, isAutheticated } from "../middleware/auth.js";
-import { getUserAnalytics } from "../controllor/analyticsControllor.js";
+import { getCourseAnalytics, getUserAnalytics } from "../controllor/analyticsControllor.js";
 
 const router = express.Router()
 
 router.get("/user-analutics",isAutheticated,isAdmin,getUserAnalytics)
+router.get("/course-analutics",isAutheticated,isAdmin,getCourseAnalytics)
 
 
 export default router;
