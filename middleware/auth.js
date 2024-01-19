1214b27545cc16ab4feee9de16d0cf2845a0d020
bikashroy120 <bikashroydt@gmail.com>
2024-01-19 12:37:19 +0600

    Bearer token add

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,51 +2,82 @@ import jwt from "jsonwebtoken";
 import ErrorHandler from "../utils/ErrorHandlers.js";
 import { catchAsyncErrors } from "./catchAsyncErrors.js";
 import dotenv from "dotenv";
-dotenv.config()
+dotenv.config();
 // import { redis } from "../utils/redis.js";
 import userModal from "../models/userModel.js";
 
+// export const isAutheticated = catchAsyncErrors(async (req, res, next) => {
+//     try {
+
+//         // ====get access token=====
+//         const accessToken = req.cookies.accessToken
+//         if (!accessToken) {
+//             return next(new ErrorHandler("Please login to access the resourse", 400))
+//         }
+
+//         /* ====verify token===== */
+//         const decodet = jwt.verify(accessToken, process.env.ACCRSS_TOKEN)
+//         if (!decodet) {
+//             return next(new ErrorHandler("access token is not valid", 400))
+//         }
+
+//         /* ====get user by token===== */
+//         const user  = await userModal.findById(decodet.id)
+//         if (!user) {
+//             return next(new ErrorHandler("user not found", 400))
+//         }
+//         /* ===== send user ===== */
+//         console.log("token ::::::", user)
+//         req.user = user
+//         next()
+//     } catch (error) {
+//         return next(new ErrorHandler(error.message, 400))
+//     }
+// })
 
 export const isAutheticated = catchAsyncErrors(async (req, res, next) => {
-    try {
+  try {
+    let token;
+    if (req?.headers?.authorization?.startsWith("Bearer")) {
+      token = req.headers.authorization.split(" ")[1];
+      try {
+        if (token) {
+          /* ====verify token===== */
+          const decodet = jwt.verify(token, process.env.ACCRSS_TOKEN);
+          if (!decodet) {
+            return next(new ErrorHandler("access token is not valid", 400));
+          }
 
-        // ====get access token=====
-        const accessToken = req.cookies.accessToken
-        if (!accessToken) {
-            return next(new ErrorHandler("Please login to access the resourse", 400))
+          /* ====get user by token===== */
+          const user = await userModal.findById(decodet.id);
+          if (!user) {
+            return next(new ErrorHandler("user not found", 400));
+          }
+          /* ===== send user ===== */
+          console.log("token ::::::", user);
+          req.user = user;
+          next();
         }
-
-
-
-        /* ====verify token===== */
-        const decodet = jwt.verify(accessToken, process.env.ACCRSS_TOKEN)
-        if (!decodet) {
-            return next(new ErrorHandler("access token is not valid", 400))
-        }
-
-        /* ====get user by token===== */
-        const user  = await userModal.findById(decodet.id)
-        if (!user) {
-            return next(new ErrorHandler("user not found", 400))
-        }
-        /* ===== send user ===== */
-        console.log("token ::::::", user)
-        req.user = user
-        next()
-    } catch (error) {
-        return next(new ErrorHandler(error.message, 400))
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 401));
+      }
+    } else {
+      res.status(401).json({ message: "Please login to access the recourse" });
     }
-})
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 401));
+  }
+});
 
-export const isAdmin = catchAsyncErrors(async(req,res,next)=>{
-    try {
-        const user = req.user;
+export const isAdmin = catchAsyncErrors(async (req, res, next) => {
+  try {
+    const user = req.user;
 
-        if(user.role!=="admin"){
-            return next(new ErrorHandler("you are not admin", 400))
-        }
-        next()
-    } catch (error) {
-        return next(new ErrorHandler(error.message, 400))
+    if (user.role !== "admin") {
+      return next(new ErrorHandler("you are not admin", 400));
     }
-})
+    next();
+  } catch (error) {
+    return next(new ErrorHandler(error.message, 400));
+  }
+});
