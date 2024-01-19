f0dd45ad7f96f81509f5c9c6e219ca62a705625e
bikashroy120 <bikashroydt@gmail.com>
2024-01-19 21:10:18 +0600

    add

--- controllor/analyticsControllor.js
+++ controllor/analyticsControllor.js
@@ -1,14 +1,14 @@
 import { catchAsyncErrors } from "../middleware/catchAsyncErrors.js";
 import userModal from "../models/userModel.js";
 import ErrorHandler from "../utils/ErrorHandlers.js";
-import { generateLast12MonthData } from "../utils/analyticsGenerator.js";
+import { generateLast12MonthData, generateLast7DaysData } from "../utils/analyticsGenerator.js";
 import courseModal from "../models/courseModel.js";
 import orderModale from "../models/orderModel.js";
 
 // get user Analyser --- for admin
 export const getUserAnalytics = catchAsyncErrors(async(req,res,next)=>{
     try {
-        const users = await generateLast12MonthData(userModal)
+        const users = await generateLast7DaysData(userModal)
 
         res.status(200).json({
             success:true,
--- utils/analyticsGenerator.js
+++ utils/analyticsGenerator.js
@@ -35,3 +35,40 @@ export async function generateLast12MonthData(Model) {
     return { last12Month}
 
 }
+
+
+export async function generateLast7DaysData(Model) {
+    const last7Days = [];
+
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 1);
+
+    for (let i = 6; i >= 0; i--) {
+        const endDate = new Date(
+            currentDate.getFullYear(),
+            currentDate.getMonth(),
+            currentDate.getDate() - i
+        );
+        const startDate = new Date(
+            endDate.getFullYear(),
+            endDate.getMonth(),
+            endDate.getDate() - 1
+        );
+
+        const dayDate = endDate.toDateString("default", {
+            day: "numeric",
+            month: "short",
+            year: "numeric"
+        });
+
+        const count = await Model.countDocuments({
+            createdAt: {
+                $gte: startDate,
+                $lt: endDate
+            }
+        });
+        last7Days.push({ day: dayDate, count });
+    }
+
+    return { last7Days };
+}
